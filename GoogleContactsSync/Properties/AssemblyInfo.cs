d88a00e1faa7cc6e5522ae5605a95fcc2eefe8f6
obelix30 <obelix30@1126ad72-2f51-4d33-8cd1-f97aedab7a97>
2016-06-13 20:52:03 +0000

    Preparation for 3.9.16

--- GoogleContactsSync/Properties/AssemblyInfo.cs
+++ GoogleContactsSync/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.9.15.*")]
-[assembly: AssemblyFileVersion("3.9.15")]
+[assembly: AssemblyVersion("3.9.16.*")]
+[assembly: AssemblyFileVersion("3.9.16")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
