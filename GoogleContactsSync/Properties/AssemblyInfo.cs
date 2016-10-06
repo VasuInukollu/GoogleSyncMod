e4d35a250fb78742edf53b85dbc9df0c71acbaf9
obelix30 <obelix30@1126ad72-2f51-4d33-8cd1-f97aedab7a97>
2016-10-06 17:34:06 +0000

    v3.10.5

--- GoogleContactsSync/Properties/AssemblyInfo.cs
+++ GoogleContactsSync/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.10.4.*")]
-[assembly: AssemblyFileVersion("3.10.4")]
+[assembly: AssemblyVersion("3.10.5.*")]
+[assembly: AssemblyFileVersion("3.10.5")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
